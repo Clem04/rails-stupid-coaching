32e06bb0c67f41ae75c05a699496e051fcdd96ab
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-17 14:12:23 -0500

    Added condition to answer method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    @answer = params[:answer]
+    correct = 'I am going to work'
+    if @question == correct
+      @answer = 'Great!'
+    elsif @question == 'I am going to work?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
   end
 end
